2c0678707e69974620b57610f62a658ae169854d
root <root@d3a2e0704417>
2017-06-01 18:15:31 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_KEPcbRDsLw",
+  "homePage" : "Main",
+  "name" : "Auto_KEPcbRDsLw",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
